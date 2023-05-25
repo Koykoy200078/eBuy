16435111f53628dd52cd601367d0e9a11d8c465e
Koykoy200078 <franc200078@gmail.com>
2023-05-25 08:33:56 +0800

    update

--- src/apps/api/auth.js
+++ src/apps/api/auth.js
@@ -1,25 +1,5 @@
 import {BASE_URI} from '../configs/url';
 
-// export function* userLogin(payload) {
-//   const options = {
-//     method: 'POST',
-//     headers: {
-//       Accept: 'application/json',
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify({...payload}),
-//   };
-
-//   const response = yield fetch(BASE_URI + '/auth/login', options);
-//   const data = yield response.json();
-
-//   if (response.ok) {
-//     return data;
-//   } else {
-//     return data;
-//   }
-// }
-
 export function* userLogin(payload) {
   try {
     const options = {
